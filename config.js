b79f29c7ab96ea8e81483db30fc0ef0a26a728b0
Rajdeep Khandelwal <khandelwalrajdeep@gmail.com>
2021-11-10 18:14:22 +0530

    config dynamoDB with aws-sdk

---
+++ config.js
@@ -0,0 +1,11 @@
+var AWS = require("aws-sdk");
+
+AWS.config.update({
+  region: "ap-south-1",
+  accessKeyId:process.env.AWS_ACCESS_KEY,
+  secretAccessKey:process.env.AWS_SECRET_KEY
+});
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export { dynamodb }
